483161ccc90b70d7b65b40094e113d5931471628
andrewborralho <andrew.borralho@gmail.com>
2015-10-04 20:38:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -58,12 +58,11 @@ server.listen(3000, onServerStart);
 var express = require('express');
 var app     = express();
 var server  = require('http').createServer(app);
-var io      = require('socket.io');
+var io      = require('socket.io').listen(server);
 io.on('connection', function(socket){
   console.log('    connection :', socket.request.connection._peername);
 });
 
-io.listen(server)
 server.listen(8000, onServerStart);
 
 
