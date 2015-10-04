f5b1b9bd1dd669528f7b4c6ca68a7560552eefa6
andrewborralho <andrew.borralho@gmail.com>
2015-10-03 21:56:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
+
 var http = require("http");
-var atendeRequisicao = function(request, response) {
+
+
+var onFirstConectionWithAirGate = function(){
+	// salvar ip
+}
+
+var getDevicesStatusFromAirGate = function(UnitId){ 
+	// query pelo modbus
+};
+var saveDevicesStatus = function(){
+	// ou db ou cache (global);
+};
+
+var getRawDataFromDb = function(){
+	return [3333, 1]; // [valor, unitId]
+};
+
+var formatRawDataFromDb = function(rawData){
+	// A ideia eh traduzir o 3333 pra alguma porcentagem
+	var formattedData = rawData;
+	return formattedData;
+};
+
+var fillDataOnHtml = function(formattedData){
+	airGateData = "<p> Valor:" + formattedData[0] + " Identificador: " + formattedData[1] + "</p>"; 
+	var html = "<html><body><h1> Dado do AirGate </h1><br>" + airGateData + "</body></html>";
+	return html;
+};
+
+var onRequest = function(request, response) {
+	console.log("	requested from " + request.connection.remoteAddress);
     response.writeHead(200, {"Content-Type": "text/html"}); // HTML??
     if (request.url == "/") {
-        response.write("nada acontece");
+    	var rawData = getRawDataFromDb();
+    	var formattedData = formatRawDataFromDb(rawData);
+    	var html = fillDataOnHtml(formattedData);
+    	response.write(html);
     }
     else if(request.url != "/") {
-        response.write("<h1>Hello Silveira!</h1>");
+        response.write("<h1>Pagina principal em ~/</h1>");
         response.end();
     }
 }
-var rotinaInicialDoServidor = function() {
-    console.log("Servidor X rodando");
+var onServerStart = function() {
+	require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  	console.log('servidor rodando em: '+add);
+	});
 }
-var server = http.createServer(atendeRequisicao);
-server.listen(3000, rotinaInicialDoServidor);
+
+var server = http.createServer(onRequest);
+server.listen(3000, onServerStart);
+
