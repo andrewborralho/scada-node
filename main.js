cee65fc31ccc79a1500357616e95c5c621f04cbb
andrewborralho <andrew.borralho@gmail.com>
2015-10-05 22:45:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
+require('colors');
+var FUNCTION_CODES = require('modbus-stack').FUNCTION_CODES;
+var ModbusRequestStack = require('modbus-stack').ModbusRequestStack;
 
+var conn = require('net').createConnection(502, '179.122.42.128');
+
+// A 'client' instance is good for 1 single request/response transaction.
+var client = new ModbusRequestStack(conn);
+
+// We're gonna call for the "Read Input Registers" function on the remote
+// MODBUS device, requesting to read the current values of the first 4 registers.
+var fc = FUNCTION_CODES.READ_HOLDING_REGISTERS;
+var startAddress = 0;
+var numToRead = 4;
+client.request(fc, startAddress, numToRead, function(err, response) {
+  if (err) {
+    throw err;
+  }
+  
+  response.forEach(function(register, i) {
+    console.log(
+      ("Sensor " + String(startAddress + i).bold + ":\t").blue +
+      (String(register/10).bold + '\u00B0F').green
+     );
+  });
+
+  // Close the connection
+  conn.end();
+});
+
+/*
 var net = require("net");
 var server = net.createServer (function (socket){ 
 	console.log("airgate conectado");
@@ -24,6 +54,8 @@ var readAirGate = function(ipAddress) {
   		client.end();
 	});
 }
+
+*/
 
 /*
 
