bc5067bb2083e6417c7f2cc6b65128932795ae60
andrewborralho <andrew.borralho@gmail.com>
2015-10-09 18:43:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,44 +5,18 @@ var clientModule = require('modbus-stack/client');
 var fs = require('fs');
 var modbus = require('modbus-stack');
 var ModbusRequestStack = require('modbus-stack').ModbusRequestStack;
+function printObject(o) {var out = '';for (var p in o) {out += p + ': ' + o[p] + '\n';}console.log(out);}
+function copyStreamParameters(source, destiny) {destiny._handle = source._handle ;destiny._hadError = source._hadError ;destiny._readableState = source._readableState ;destiny.readable = source.readable ;destiny._events = source._events ;destiny._eventsCount = source._eventsCount ;destiny._writableState = source._writableState ;destiny.writable = source.writable ;destiny.allowHalfOpen = source.allowHalfOpen ;destiny.destroyed = source.destroyed ;destiny.bytesRead = source.bytesRead ;destiny._bytesDispatched = source._bytesDispatched ;destiny._pendingEncoding = source._pendingEncoding ;destiny._peername = source._peername ;destiny._unrefTimer = source._unrefTimer ;}
+console.log("");console.log(" ---- ----------------------------- ---- ");
 
-
-console.log("");
-console.log(" ---- ----------------------------- ---- ");
-
-function printObject(o) {
-  var out = '';
-  for (var p in o) {
-    out += p + ': ' + o[p] + '\n';
-  }
-  console.log(out);
-}
-
-function copyStreamParameters(source, destiny) {
-	destiny._handle = source._handle ;
-	destiny._hadError = source._hadError ;
-	destiny._readableState = source._readableState ;
-	destiny.readable = source.readable ;
-	destiny._events = source._events ;
-	destiny._eventsCount = source._eventsCount ;
-	destiny._writableState = source._writableState ;
-	destiny.writable = source.writable ;
-	destiny.allowHalfOpen = source.allowHalfOpen ;
-	destiny.destroyed = source.destroyed ;
-	destiny.bytesRead = source.bytesRead ;
-	destiny._bytesDispatched = source._bytesDispatched ;
-	destiny._pendingEncoding = source._pendingEncoding ;
-	destiny._peername = source._peername ;
-	destiny._unrefTimer = source._unrefTimer ;
-}
 
 var callAirGate = function(socket){
-	console.log("tentando enviar HRH para: " + socket.remoteAddress + ":" + socket.remotePort);
+	console.log("tentando enviar RHR para: " + socket.remoteAddress + ":" + socket.remotePort);
 	//printObject(socket);
 	var client = new ModbusRequestStack(socket);
 
 	var gotResponse = false;
-	copyStreamParameters(socket, client);
+	// copyStreamParameters(socket, client);
 	
 	client.request(RHR, 0, 5, function(err, response) {
   		if (err) {console.log(err);throw err;};
@@ -85,7 +59,7 @@ var server = net.createServer (function (socket){
 		catch(err) {
 			console.log("airgate error: " + err)
 		}
-	},6000);
+	},3000);
 	
 }, 10000);
 
