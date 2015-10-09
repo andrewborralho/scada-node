823453d7f8b28bd9d34b59b8d36b2b4316a82f8d
andrewborralho <andrew.borralho@gmail.com>
2015-10-09 17:26:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ var callAirGate = function(socket){
 	console.log("tentando enviar mensagem para: " + socket.remoteAddress + ":" + socket.remotePort);
 	var client = new modbus.ModbusResponseStack(socket);
 	var gotResponse = false;
-	/*
+	
 	client.prototype.request = function() {
   		var req = new modbus.ModbusRequestStack(this);
   		req.request.apply(req, arguments);
   		return req;
 	}
-	*/
+	
 	
 	client.request(RHR, 0, 5, function(err, response) {
   		if (err) {console.log(err);throw err;};
