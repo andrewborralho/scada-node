a77ef091e29703e55a50965f1c401004a539c599
andrewborralho <andrew.borralho@gmail.com>
2015-10-11 18:34:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -26,18 +26,18 @@ console.log("[0].values " + res[0].values);
 
 */
 console.log('agora apaga tudo');
-db.run("DELETE FROM modhistory;");
+db.run("DELETE FROM modhistory");
 
 console.log('nao eh pra vir nada');
-var res = db.exec("SELECT * FROM modhistory;");
+var res = db.exec("SELECT * FROM modhistory");
 console.log("[0].columns " + res[0].columns);
 console.log("[0].values " + res[0].values);
 
 console.log('reinsere');
-db.run("INSERT INTO modhistory VALUES (?,?), (?,?);", [1,3000,2,6000]);
+db.run("INSERT INTO modhistory VALUES (?,?), (?,?)", [1,3000,2,6000]);
 
 console.log('ve valores inseridos');
-var res = db.exec("SELECT * FROM modhistory;");
+var res = db.exec("SELECT * FROM modhistory");
 console.log("[0].columns " + res[0].columns);
 console.log("[0].values " + res[0].values);
 
